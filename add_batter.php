794fde4a96e4cc5966f76d9add107c6419b2fb6d
Cody Crow <cody@cms07.org>
2014-03-17 09:57:06 -0500

    First commit
    Ability to add batters and their stats.
    Ability to view statistical information.

---
+++ add_batter.php
@@ -0,0 +1,125 @@
+<?php
+
+$db = new PDO('mysql:host=localhost;dbname=baseball;charset=utf8', 'root', '');
+
+if (htmlspecialchars($_GET["sent"])) {
+	
+	$_player = $_GET["player"];
+	$_pa = $_GET["pa"];
+	$_h = $_GET["h"];
+	$_bb = $_GET["bb"];
+	$_so = $_GET["so"];
+	$_hbp = $_GET["hbp"];
+	$_2b = $_GET["2b"];
+	$_3b = $_GET["3b"];
+	$_hr = $_GET["hr"];
+	$_rbi = $_GET["rbi"];
+	$_sac = $_GET["sac"];
+	$_r = $_GET["r"];
+	$_sb = $_GET["sb"];
+	$_cs = $_GET["cs"];
+	$_ob = $_GET["ob"];
+	
+
+	//Process player if it matches one already in the database.
+	$stmt = $db->prepare('SELECT * FROM batting WHERE player = ?');
+	
+	if ($stmt->execute(array($_GET['player'])) && $stmt->rowCount()) {
+		while ($player = $stmt->fetch()) {
+			$_pa += $player['pa'];
+			$_bb += $player['bb'];
+			$_so += $player['so'];
+			$_hbp += $player['hbp'];
+			$_2b += $player['2b'];
+			$_3b += $player['3b'];
+			$_hr += $player['hr'];
+			$_rbi += $player['rbi'];
+			$_sac += $player['sac'];
+			$_r += $player['r'];
+			$_sb += $player['sb'];
+			$_cs += $player['cs'];
+			$_ob += $player['ob'];
+			
+			$clearold = $db->prepare("DELETE FROM `batting` WHERE `player` = ? AND `pa` = ?");
+			$clearold->execute(array($player['player'], $player['pa']));	
+			
+			
+			$stmt2 = $db->prepare("INSERT INTO batting (player, pa, h, bb, so, hbp, 2b, 3b, hr, rbi, sac, r, sb, cs, ob) VALUES (:player, :pa, :h, :bb, :so, :hbp, :2b, :3b, :hr, :rbi, :sac, :r, :sb, :cs, :ob)");
+			$stmt2->bindParam(':player', $_player);
+			$stmt2->bindParam(':pa', $_pa);
+			$stmt2->bindParam(':h', $_h);
+			$stmt2->bindParam(':bb', $_bb);
+			$stmt2->bindParam(':so', $_so);
+			$stmt2->bindParam(':hbp', $_hbp);
+			$stmt2->bindParam(':2b', $_2b);
+			$stmt2->bindParam(':3b', $_3b);
+			$stmt2->bindParam(':hr', $_hr);
+			$stmt2->bindParam(':rbi', $_rbi);
+			$stmt2->bindParam(':sac', $_sac);
+			$stmt2->bindParam(':r', $_r);
+			$stmt2->bindParam(':sb', $_sb);
+			$stmt2->bindParam(':cs', $_cs);
+			$stmt2->bindParam(':ob', $_ob);
+			$stmt2->execute();
+			echo "Existing.";
+		}
+	} else {
+			$stmt2 = $db->prepare("INSERT INTO batting (player, pa, h, bb, so, hbp, 2b, 3b, hr, rbi, sac, r, sb, cs, ob) VALUES (:player, :pa, :h, :bb, :so, :hbp, :2b, :3b, :hr, :rbi, :sac, :r, :sb, :cs, :ob)");
+			$stmt2->bindParam(':player', $_player);
+			$stmt2->bindParam(':pa', $_pa);
+			$stmt2->bindParam(':h', $_h);
+			$stmt2->bindParam(':bb', $_bb);
+			$stmt2->bindParam(':so', $_so);
+			$stmt2->bindParam(':hbp', $_hbp);
+			$stmt2->bindParam(':2b', $_2b);
+			$stmt2->bindParam(':3b', $_3b);
+			$stmt2->bindParam(':hr', $_hr);
+			$stmt2->bindParam(':rbi', $_rbi);
+			$stmt2->bindParam(':sac', $_sac);
+			$stmt2->bindParam(':r', $_r);
+			$stmt2->bindParam(':sb', $_sb);
+			$stmt2->bindParam(':cs', $_cs);
+			$stmt2->bindParam(':ob', $_ob);
+			$stmt2->execute();
+			echo "New Player";
+	}
+	echo "<br /><span style='color:#0a0;font-weight:900;text-size:18px;'>Player stats added.</span><br />";
+}
+
+
+
+?>
+<form action="add_batter.php" method="get">
+Player Name: <br />
+<input type="text" name="player" /><br />
+Plate Appearances: <br />
+<input type="text" name="pa" /><br />
+Hits: <br />
+<input type="text" name="h" /><br />
+Walks: <br />
+<input type="text" name="bb" /><br />
+Strikeouts: <br />
+<input type="text" name="so" /><br />
+Hit by Pitch: <br />
+<input type="text" name="hbp" /><br />
+Doubles: <br />
+<input type="text" name="2b" /><br />
+Triples: <br />
+<input type="text" name="3b" /><br />
+Homeruns: <br />
+<input type="text" name="hr" /><br />
+RBI: <br />
+<input type="text" name="rbi" /><br />
+Sacrifices: <br />
+<input type="text" name="sac" /><br />
+Runs: <br />
+<input type="text" name="r" /><br />
+Stolen Bases: <br />
+<input type="text" name="sb" /><br />
+Caught Stealing: <br />
+<input type="text" name="cs" /><br />
+Times on Base: <br />
+<input type="text" name="ob" /><br />
+<input type="hidden" name="sent" value="1" />
+<input type="submit" />
+</form>
---
+++ stats.php
@@ -0,0 +1,200 @@
+<?php
+
+
+$db = new PDO('mysql:host=localhost;dbname=baseball;charset=utf8', 'root', '');
+
+echo "
+<h1>Comanche Statistics</h1>
+<h2>Batting Statistics</h2>
+<table>
+	<thead>";
+
+echo "<tr style='text-decoration:underline;'>";
+	echo "<td>Player Name</td>";
+	echo "<td>OBP</td>";
+	echo "<td>AVG</td>";
+	echo "<td>SLG</td>";
+	echo "<td>OPS</td>";
+	echo "<td>Runs Created</td>";
+	echo "<td>PA</td>";
+	echo "<td>Hits</td>";
+	echo "<td>Walks</td>";
+	echo "<td>Strikeouts</td>";
+	echo "<td>Total Bases</td>";
+	echo "<td>HBP</td>";
+	echo "<td>2B</td>";
+	echo "<td>3B</td>";
+	echo "<td>HR</td>";
+	echo "<td>RBI</td>";
+	echo "<td>Sacrifices</td>";
+	echo "<td>Runs</td>";
+	echo "<td>SB</td>";
+	echo "<td>CS</td>";
+	echo "<td>Stolen Base %</td>";
+	echo "<td>Times on Base</td>";
+	echo "</tr>";
+
+echo"
+	</thead>
+";
+
+foreach($db->query('SELECT * FROM batting') as $player) {
+	//Calculations
+	
+	//OBP = OB / PA
+	$obp = ($player['ob']) / $player['pa'];
+	
+	//At Bats
+	$ab = $player['pa'] - ($player['bb'] + $player['hbp'] + $player['sac']);
+	
+	//Batting Avg = Hits / At Bats
+	$avg = $player['h'] / $ab;
+	
+	//Total Bases = (1*1B + 2*2B + 3*3B + 4*HR);
+	$tb = ($player['h'] - ($player['2b'] + $player['3b'] + $player['hr'])) + (2 * $player['2b']) + (3 * $player['3b']) + (4 * $player['hr']); 
+	
+	//Slugging Percentage = Total Bases / At Bats
+	$slg = $tb / $ab;
+	
+	//OPS = On Base Percentage + Slugging Percentage
+	$ops = $obp + $slg;
+	
+	//Runs Created = (Hits + BB)(Total Bases) / (AB + BB)
+	$rc = ($player['h'] + $player['bb']) / ($ab + $player['bb']);
+	
+	//Stolen Base Percentage = SB / (SB + CS)
+	$sbp = $player['sb'] / ($player['sb'] + $player['cs']);
+	
+	
+	echo "<tr>";
+	echo "<td>" . $player['player'] . "</td>";
+	echo "<td>";
+	printf("%.3f <br />", $obp); //OBP to three decimal places
+	echo "</td>";
+	echo "<td>";
+	printf("%.3f <br />", $avg); //AVG to three decimal places
+	echo "</td>";
+	echo "<td>";
+	printf("%.3f <br />", $slg); //SLG to three decimal places
+	echo "</td>";
+	echo "<td>";
+	printf("%.3f <br />", $ops); //OPS to three decimal places
+	echo "</td>";
+	echo "<td>";
+	printf("%.3f <br />", $rc); //RC to three decimal places
+	echo "</td>";
+	echo "<td>" . $player['pa'] . "</td>";
+	echo "<td>" . $player['h'] . "</td>";
+	echo "<td>" . $player['bb'] . "</td>";
+	echo "<td>" . $player['so'] . "</td>";
+	echo "<td>" . $tb . "</td>";
+	echo "<td>" . $player['hbp'] . "</td>";
+	echo "<td>" . $player['2b'] . "</td>";
+	echo "<td>" . $player['3b'] . "</td>";
+	echo "<td>" . $player['hr'] . "</td>";
+	echo "<td>" . $player['rbi'] . "</td>";
+	echo "<td>" . $player['sac'] . "</td>";
+	echo "<td>" . $player['r'] . "</td>";
+	echo "<td>" . $player['sb'] . "</td>";
+	echo "<td>" . $player['cs'] . "</td>";
+	echo "<td>";
+	printf("%.3f <br />", $sbp); //SBP to three decimal places
+	echo "</td>";
+	echo "<td>" . $player['ob'] . "</td>";
+	echo "</tr>";
+}
+
+echo "</table>";
+
+echo "<h2>Pitching Statistics</h2>";
+
+echo "<table>";
+echo "<thead>";
+		echo "<tr style='text-decoration:underline;'>";
+	echo "<td>Player Name</td>";
+	echo "<td>ERA</td>";
+	echo "<td>Wins</td>";
+	echo "<td>Losses</td>";
+	echo "<td>Innings Pitched</td>";
+	echo "<td>WHIP</td>";
+	echo "<td>W-L %</td>";
+	echo "<td>H/7</td>";
+	echo "<td>BB/7</td>";
+	echo "<td>SO/7</td>";
+	echo "<td>SO/BB</td>";
+	echo "<td>Hits Allowed</td>";
+	echo "<td>Walks</td>";
+	echo "<td>Hit Batsmen</td>";
+	echo "<td>Earned Runs</td>";
+	echo "<td>Strikeouts</td>";
+	echo "<td>Games</td>";
+	echo "<td>Saves</td>";
+	echo "<td>Batters Faced</td>";
+	echo "</tr>";
+echo "</thead>";
+
+
+foreach($db->query('SELECT * FROM pitching') as $player) {
+	//Calculations
+	
+	//Earned Run Average = (Earned Runs / IP) * 7
+	$era = $player['er'] / $player['ip'] * 7.0;
+	
+	//Walks & Hits per Innings Pitched = (BB + H)/IP
+	$whip = ($player['bb'] + $player['h']) / $player['ip'];
+	
+	//Win-loss Percentage = Wins / Decisions
+	$wlpercentage = $player['w'] / ($player['l'] + $player['w']);
+	
+	//Hits per 7 = 7 * (H / IP)
+	$h7 = 7 * ($player['h'] / $player['ip']);
+	
+	//Walks per 7 = 7 * (BB/IP)
+	$bb7 = 7 * ($player['bb'] / $player['ip']);
+	
+	//Strikeouts per 7 = 7 * (k / ip);
+	$so7 = 7 * ($player['k'] / $player['ip']);
+	
+	//Strikeouts / Walks
+	$sobb = $player['k'] / $player['bb'];
+	
+		echo "<tr>";
+	echo "<td>" . $player['player'] . "</td>";
+	echo "<td>";
+	printf("%.3f <br />", $era); //ERA to three decimal places
+	echo "</td>";
+	echo "<td>" . $player['w'] . "</td>";
+	echo "<td>" . $player['l'] . "</td>";
+	echo "<td>" . $player['ip'] . "</td>";
+	echo "<td>";
+	printf("%.3f <br />", $whip); //WHIP to three decimal places
+	echo "</td>";
+	echo "<td>";
+	printf("%.3f <br />", $wlpercentage); //W-L% to three decimal places
+	echo "</td>";
+	echo "<td>";
+	printf("%.3f <br />", $h7); //H/7 to three decimal places
+	echo "</td>";
+	echo "<td>";
+	printf("%.3f <br />", $bb7); //BB/7 to three decimal places
+	echo "</td>";
+	echo "<td>";
+	printf("%.3f <br />", $so7); //SO/7 to three decimal places
+	echo "</td>";
+	echo "<td>";
+	printf("%.3f <br />", $sobb); //SO/BB to three decimal places
+	echo "</td>";
+	echo "<td>" . $player['h'] . "</td>";
+	echo "<td>" . $player['bb'] . "</td>";
+	echo "<td>" . $player['hbp'] . "</td>";
+	echo "<td>" . $player['er'] . "</td>";
+	echo "<td>" . $player['k'] . "</td>";
+	echo "<td>" . $player['g'] . "</td>";
+	echo "<td>" . $player['s'] . "</td>";
+	echo "<td>" . $player['bf'] . "</td>";
+	echo "</tr>";
+}
+
+echo "</table>";
+
+?>
